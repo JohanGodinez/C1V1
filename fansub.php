d46b29a0a881f957e48e4b1b24bf5636e97b149b
Johan Godinez <johang77@hotmail.com>
2017-01-27 17:47:35 -0600

    Fansub

--- fansub.php
+++ fansub.php
@@ -1,3 +1,21 @@
 <?php require 'head.php' ?>
-
+<section class="app-section2">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-10 col-sm-offset-1 col-md-2 col-md-offset-5">
+                <br />
+                <h2 class="text-center etiqueta">Fansub</h2>
+                <br />
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-xs-12 col-sm-6 col-md-6 col-md-offset-3">
+                <p class="text-justify fansub">Somos un grupo de fans del <span class="app-resaltar">manga/anime</span> de diversos países que han decidido trabajar en equipo para sacar diversas
+                    traducciones a mangas olvidados en este lado del mundo, de momento estamos trabajando en pocos proyectos, 
+                    pero con el tiempo esperamos tener varios mas para ofrecer a la gente que nos siga.
+                </p>
+            </figure>
+        </div>
+    </div>
+</section>
 <?php require 'footer.php' ?>
