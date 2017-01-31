e8a5cf85c54269ad2c3a6b1ac3ed60c4a784752f
Johan Godinez <johang77@hotmail.com>
2017-01-31 17:43:06 -0600

    Update index.php
    actualizando slider

--- index.php
+++ index.php
@@ -5,19 +5,19 @@
     <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
         <div class="carousel-inner" role="listbox">
             <div class="item active">
-            <img src="Img/Slider/img1.png" alt="...">
+            <img src="Img/Slider/001a.png" alt="...">
             </div>
             <div class="item">
-            <img src="Img/Slider/img2.png" alt="...">
+            <img src="Img/Slider/002a.png" alt="...">
             </div>
             <div class="item">
-            <img src="Img/Slider/img3.png" alt="...">
+            <img src="Img/Slider/003a.png" alt="...">
             </div>
             <div class="item">
-            <img src="Img/Slider/img4.png" alt="...">
+            <img src="Img/Slider/004a.png" alt="...">
             </div>
             <div class="item">
-            <img src="Img/Slider/img5.png" alt="...">
+            <img src="Img/Slider/005a.png" alt="...">
             </div>
         </div>
     </div>
