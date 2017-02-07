a4f5f71f6a77d005b5a221a71f6fde12fb57aac4
Johan Godinez <johang77@hotmail.com>
2017-02-06 19:35:06 -0600

    Update manga.php
    ajustando imagenes

--- manga.php
+++ manga.php
@@ -4,100 +4,100 @@
     <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
         <div class="carousel-inner" role="listbox">
             <div class="item active">
-            <img src="Img/Toumei/Toumei%201/img000002.png" alt="...">
+            <img src="Img/Toumei/Toumei%201/img000002.png" alt="..." class="img-responsive">
             </div>
             <div class="item">
-            <img src="Img/Toumei/Toumei%201/img000003.png" alt="...">
+            <img src="Img/Toumei/Toumei%201/img000003.png" alt="..." class="img-responsive">
             </div>
             <div class="item">
-            <img src="Img/Toumei/Toumei%201/img000004.png" alt="...">
+            <img src="Img/Toumei/Toumei%201/img000004.png" alt="..." class="img-responsive">
             </div>
             <div class="item">
-            <img src="Img/Toumei/Toumei%201/img000005.png" alt="...">
+            <img src="Img/Toumei/Toumei%201/img000005.png" alt="..." class="img-responsive">
             </div>
             <div class="item">
-            <img src="Img/Toumei/Toumei%201/img000006.png" alt="...">
+            <img src="Img/Toumei/Toumei%201/img000006.png" alt="..." class="img-responsive">
             </div>
             <div class="item">
-            <img src="Img/Toumei/Toumei%201/img000007.png" alt="...">
+            <img src="Img/Toumei/Toumei%201/img000007.png" alt="..." class="img-responsive">
             </div>
             <div class="item">
-            <img src="Img/Toumei/Toumei%201/img000008.png" alt="...">
+            <img src="Img/Toumei/Toumei%201/img000008.png" alt="..." class="img-responsive">
             </div>
             <div class="item">
-            <img src="Img/Toumei/Toumei%201/img000009.png" alt="...">
+            <img src="Img/Toumei/Toumei%201/img000009.png" alt="..." class="img-responsive">
             </div>
             <div class="item">
-            <img src="Img/Toumei/Toumei%201/img000010.png" alt="...">
+            <img src="Img/Toumei/Toumei%201/img000010.png" alt="..." class="img-responsive">
             </div>
             <div class="item">
-            <img src="Img/Toumei/Toumei%201/img000011.png" alt="...">
+            <img src="Img/Toumei/Toumei%201/img000011.png" alt="..." class="img-responsive">
             </div>
             <div class="item">
-            <img src="Img/Toumei/Toumei%201/img000012.png" alt="...">
+            <img src="Img/Toumei/Toumei%201/img000012.png" alt="..." class="img-responsive">
             </div>
             <div class="item">
-            <img src="Img/Toumei/Toumei%201/img000013.png" alt="...">
+            <img src="Img/Toumei/Toumei%201/img000013.png" alt="..." class="img-responsive">
             </div>
             <div class="item">
-            <img src="Img/Toumei/Toumei%201/img000014.png" alt="...">
+            <img src="Img/Toumei/Toumei%201/img000014.png" alt="..." class="img-responsive">
             </div>
             <div class="item">
-            <img src="Img/Toumei/Toumei%201/img000015.png" alt="...">
+            <img src="Img/Toumei/Toumei%201/img000015.png" alt="..." class="img-responsive">
             </div>
             <div class="item">
-            <img src="Img/Toumei/Toumei%201/img000016.png" alt="...">
+            <img src="Img/Toumei/Toumei%201/img000016.png" alt="..." class="img-responsive">
             </div>
             <div class="item">
-            <img src="Img/Toumei/Toumei%201/img000017.png" alt="...">
+            <img src="Img/Toumei/Toumei%201/img000017.png" alt="..." class="img-responsive">
             </div>
             <div class="item">
-            <img src="Img/Toumei/Toumei%201/img000018.png" alt="...">
+            <img src="Img/Toumei/Toumei%201/img000018.png" alt="..." class="img-responsive">
             </div>
             <div class="item">
-            <img src="Img/Toumei/Toumei%201/img000019.png" alt="...">
+            <img src="Img/Toumei/Toumei%201/img000019.png" alt="..." class="img-responsive">
             </div>
             <div class="item">
-            <img src="Img/Toumei/Toumei%201/img000020.png" alt="...">
+            <img src="Img/Toumei/Toumei%201/img000020.png" alt="..." class="img-responsive">
             </div>
             <div class="item">
-            <img src="Img/Toumei/Toumei%201/img000021.png" alt="...">
+            <img src="Img/Toumei/Toumei%201/img000021.png" alt="..." class="img-responsive">
             </div>
             <div class="item">
-            <img src="Img/Toumei/Toumei%201/img000022.png" alt="...">
+            <img src="Img/Toumei/Toumei%201/img000022.png" alt="..." class="img-responsive">
             </div>
             <div class="item">
-            <img src="Img/Toumei/Toumei%201/img000023.png" alt="...">
+            <img src="Img/Toumei/Toumei%201/img000023.png" alt="..." class="img-responsive">
             </div>
             <div class="item">
-            <img src="Img/Toumei/Toumei%201/img000024.png" alt="...">
+            <img src="Img/Toumei/Toumei%201/img000024.png" alt="..." class="img-responsive">
             </div>
             <div class="item">
-            <img src="Img/Toumei/Toumei%201/img000025.png" alt="...">
+            <img src="Img/Toumei/Toumei%201/img000025.png" alt="..." class="img-responsive">
             </div>
             <div class="item">
-            <img src="Img/Toumei/Toumei%201/img000026.png" alt="...">
+            <img src="Img/Toumei/Toumei%201/img000026.png" alt="..." class="img-responsive">
             </div>
             <div class="item">
-            <img src="Img/Toumei/Toumei%201/img000027.png" alt="...">
+            <img src="Img/Toumei/Toumei%201/img000027.png" alt="..." class="img-responsive">
             </div>
             <div class="item">
-            <img src="Img/Toumei/Toumei%201/img000028.png" alt="...">
+            <img src="Img/Toumei/Toumei%201/img000028.png" alt="..." class="img-responsive">
             </div>
             <div class="item">
-            <img src="Img/Toumei/Toumei%201/img000029.png" alt="...">
+            <img src="Img/Toumei/Toumei%201/img000029.png" alt="..." class="img-responsive">
             </div>
             <div class="item">
-            <img src="Img/Toumei/Toumei%201/img000030.png" alt="...">
+            <img src="Img/Toumei/Toumei%201/img000030.png" alt="..." class="img-responsive">
             </div>
             <div class="item">
-            <img src="Img/Toumei/Toumei%201/img000031.png" alt="...">
+            <img src="Img/Toumei/Toumei%201/img000031.png" alt="..." class="img-responsive">
             </div>
             <div class="item">
-            <img src="Img/Toumei/Toumei%201/img000032.png" alt="...">
+            <img src="Img/Toumei/Toumei%201/img000032.png" alt="..." class="img-responsive">
             </div>
             <div class="item">
-            <img src="Img/Toumei/Toumei%201/img000033.png" alt="...">
+            <img src="Img/Toumei/Toumei%201/img000033.png" alt="..." class="img-responsive">
             </div>
             
              <!-- Controls -->
