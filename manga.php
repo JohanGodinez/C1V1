cbbb25b7c64707101a0d2f6f2a0023a4a7bc3d34
Johan Godinez <johang77@hotmail.com>
2017-02-06 19:47:17 -0600

    Update manga.php

--- manga.php
+++ manga.php
@@ -1,6 +1,6 @@
 <?php require "head.php";?>
 
-<section class="col-md-6 col-md-offset-3">
+<section class="">
     <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
         <div class="carousel-inner" role="listbox">
             <div class="item active">
