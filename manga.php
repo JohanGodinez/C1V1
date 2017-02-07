36d91cb2908b36e37ca4fe32c8d41a8ea0b8e651
Johan Godinez <johang77@hotmail.com>
2017-02-06 18:51:18 -0600

    Update manga.php
    Control carrousel

--- manga.php
+++ manga.php
@@ -18,6 +18,16 @@
             <div class="item">
             <img src="Img/Slider/img5.png" alt="...">
             </div>
+            
+             <!-- Controls -->
+              <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+                <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+              </a>
+              <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+                <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+              </a>
         </div>
     </div>
 </section>
