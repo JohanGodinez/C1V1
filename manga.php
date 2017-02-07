655bae6ff9b2f29deb855cd99dc7313e3b65635a
Johan Godinez <johang77@hotmail.com>
2017-02-06 19:07:36 -0600

    Update manga.php
    Mangas

--- manga.php
+++ manga.php
@@ -4,19 +4,103 @@
     <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
         <div class="carousel-inner" role="listbox">
             <div class="item active">
-            <img src="Img/Slider/img1.png" alt="...">
+            <img src="Img/Toumei/Toumei%201/img000001.png" alt="...">
             </div>
-            <div class="item">
-            <img src="Img/Slider/img2.png" alt="...">
+            <div class="item active">
+            <img src="Img/Toumei/Toumei%201/img000002.png" alt="...">
             </div>
-            <div class="item">
-            <img src="Img/Slider/img3.png" alt="...">
+            <div class="item active">
+            <img src="Img/Toumei/Toumei%201/img000003.png" alt="...">
             </div>
-            <div class="item">
-            <img src="Img/Slider/img4.png" alt="...">
+            <div class="item active">
+            <img src="Img/Toumei/Toumei%201/img000004.png" alt="...">
             </div>
-            <div class="item">
-            <img src="Img/Slider/img5.png" alt="...">
+            <div class="item active">
+            <img src="Img/Toumei/Toumei%201/img000005.png" alt="...">
+            </div>
+            <div class="item active">
+            <img src="Img/Toumei/Toumei%201/img000006.png" alt="...">
+            </div>
+            <div class="item active">
+            <img src="Img/Toumei/Toumei%201/img000007.png" alt="...">
+            </div>
+            <div class="item active">
+            <img src="Img/Toumei/Toumei%201/img000008.png" alt="...">
+            </div>
+            <div class="item active">
+            <img src="Img/Toumei/Toumei%201/img000009.png" alt="...">
+            </div>
+            <div class="item active">
+            <img src="Img/Toumei/Toumei%201/img000010.png" alt="...">
+            </div>
+            <div class="item active">
+            <img src="Img/Toumei/Toumei%201/img000011.png" alt="...">
+            </div>
+            <div class="item active">
+            <img src="Img/Toumei/Toumei%201/img000012.png" alt="...">
+            </div>
+            <div class="item active">
+            <img src="Img/Toumei/Toumei%201/img000013.png" alt="...">
+            </div>
+            <div class="item active">
+            <img src="Img/Toumei/Toumei%201/img000014.png" alt="...">
+            </div>
+            <div class="item active">
+            <img src="Img/Toumei/Toumei%201/img000015.png" alt="...">
+            </div>
+            <div class="item active">
+            <img src="Img/Toumei/Toumei%201/img000016.png" alt="...">
+            </div>
+            <div class="item active">
+            <img src="Img/Toumei/Toumei%201/img000017.png" alt="...">
+            </div>
+            <div class="item active">
+            <img src="Img/Toumei/Toumei%201/img000018.png" alt="...">
+            </div>
+            <div class="item active">
+            <img src="Img/Toumei/Toumei%201/img000019.png" alt="...">
+            </div>
+            <div class="item active">
+            <img src="Img/Toumei/Toumei%201/img000020.png" alt="...">
+            </div>
+            <div class="item active">
+            <img src="Img/Toumei/Toumei%201/img000021.png" alt="...">
+            </div>
+            <div class="item active">
+            <img src="Img/Toumei/Toumei%201/img000022.png" alt="...">
+            </div>
+            <div class="item active">
+            <img src="Img/Toumei/Toumei%201/img000023.png" alt="...">
+            </div>
+            <div class="item active">
+            <img src="Img/Toumei/Toumei%201/img000024.png" alt="...">
+            </div>
+            <div class="item active">
+            <img src="Img/Toumei/Toumei%201/img000025.png" alt="...">
+            </div>
+            <div class="item active">
+            <img src="Img/Toumei/Toumei%201/img000026.png" alt="...">
+            </div>
+            <div class="item active">
+            <img src="Img/Toumei/Toumei%201/img000027.png" alt="...">
+            </div>
+            <div class="item active">
+            <img src="Img/Toumei/Toumei%201/img000028.png" alt="...">
+            </div>
+            <div class="item active">
+            <img src="Img/Toumei/Toumei%201/img000029.png" alt="...">
+            </div>
+            <div class="item active">
+            <img src="Img/Toumei/Toumei%201/img000030.png" alt="...">
+            </div>
+            <div class="item active">
+            <img src="Img/Toumei/Toumei%201/img000031.png" alt="...">
+            </div>
+            <div class="item active">
+            <img src="Img/Toumei/Toumei%201/img000032.png" alt="...">
+            </div>
+            <div class="item active">
+            <img src="Img/Toumei/Toumei%201/img000033.png" alt="...">
             </div>
             
              <!-- Controls -->
