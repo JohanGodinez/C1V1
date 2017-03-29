4411395b9a4adce371862fc372f881693b79b14b
Johan Godinez <johang77@hotmail.com>
2017-03-28 20:24:26 -0600

    Update proyectos.php

--- proyectos.php
+++ proyectos.php
@@ -1,7 +1,8 @@
 <?php
     require 'head.php'
 ?>
-<div class="col-md-12">
+<div class="col-md-3 right">
+    <label class="control-label">Buscar : </label>
     <input type="text" class="form-control" name="buscar" placeholder="Titulo" ng-model="search.titulo" >
 </div>
 <div ng-controller="ProyectoController">
